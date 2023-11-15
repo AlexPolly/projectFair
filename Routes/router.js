e8d982b4d87c72496ef8482c772f21a8bffc8cd0
AlexPolly <alexpolly@gmail.com>
2023-11-15 11:36:00 +0530

    project fair

---
+++ Routes/router.js
@@ -0,0 +1,13 @@
+// import express
+const express = require('express')
+const router = new express.Router()
+const userController = require('../Controllers/userController')
+
+// register API
+router.post('/user/register',userController.register)
+
+// login
+router.post('/user/login',userController.login)
+
+// export router
+module.exports = router
